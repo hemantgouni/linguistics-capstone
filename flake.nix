dbb9be367039351387b02609511ca671dc3fa734
Hemant Sai Gouni <hemant@hemantgouni.com>
2023-02-25 22:48:02 +0000

    Add basic intro, some discussion on approach

---
+++ flake.nix
@@ -0,0 +1,25 @@
+{
+  description = "A devShell example";
+
+  inputs = {
+    nixpkgs.url      = "github:nixos/nixpkgs/nixos-unstable";
+    flake-utils.url  = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils, ... }:
+  flake-utils.lib.eachDefaultSystem
+    (system:
+      let pkgs = nixpkgs.legacyPackages.${system}; in
+      {
+        devShells.default = pkgs.mkShell {
+          buildInputs = [
+            pkgs.openssl
+            pkgs.pkgconfig
+            pkgs.rustc
+            pkgs.cargo
+            pkgs.rust-analyzer
+          ];
+        };
+      }
+    );
+}
